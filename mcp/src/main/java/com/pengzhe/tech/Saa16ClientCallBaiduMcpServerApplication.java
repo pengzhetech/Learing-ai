05ef4efa99b510026b43909dea1d6de3740f3994
pengzhe <18900433@qq.com>
2025-12-12 21:49:02 +0800

    mcp

---
+++ mcp/src/main/java/com/pengzhe/tech/Saa16ClientCallBaiduMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.pengzhe.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Saa16ClientCallBaiduMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Saa16ClientCallBaiduMcpServerApplication.class, args);
+    }
+
+}
