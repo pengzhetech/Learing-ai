7a46d791a25c70855ca84bc716fe247889e9920d
pengzhe <18900433@qq.com>
2025-12-13 12:11:38 +0800

    prompt template

--- structured-output/src/main/java/com/pengzhe/tech/Book.java
+++ structured-output/src/main/java/com/pengzhe/tech/Book.java
@@ -5,40 +5,33 @@ import lombok.Data;
 import java.util.Objects;
 
 /**
- * @auther zzyybs@126.com
  * @create 2025-07-27 21:01
  * @Description 传统entity
  */
 @Data
-public class Book
-{
+public class Book {
     //Field
     int id;
     String bookName;
 
-    public Book()
-    {
+    public Book() {
     }
 
-    public Book(int id, String bookName)
-    {
+    public Book(int id, String bookName) {
         this.id = id;
         this.bookName = bookName;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
 
-    public void setId(int id)
-    {
+    public void setId(int id) {
         this.id = id;
     }
 
     @Override
-    public boolean equals(Object o)
-    {
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
@@ -46,17 +39,12 @@ public class Book
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return Objects.hash(id, bookName);
     }
 
     @Override
-    public String toString()
-    {
-        return "Book{" +
-                "id=" + id +
-                ", bookName='" + bookName + '\'' +
-                '}';
+    public String toString() {
+        return "Book{" + "id=" + id + ", bookName='" + bookName + '\'' + '}';
     }
 }
