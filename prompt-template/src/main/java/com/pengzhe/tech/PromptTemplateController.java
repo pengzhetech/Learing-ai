d9464e8c021b1d735e1f8788e91cfbe9ce4c2dee
pengzhe <18900433@qq.com>
2025-12-13 12:05:38 +0800

    prompt template

--- prompt-template/src/main/java/com/pengzhe/tech/PromptTemplateController.java
+++ prompt-template/src/main/java/com/pengzhe/tech/PromptTemplateController.java
@@ -17,60 +17,27 @@ import reactor.core.publisher.Flux;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @auther zzyybs@126.com
- * @create 2025-07-26 16:25
- * @Description TODO
- */
 @RestController
-public class PromptTemplateController
-{
+public class PromptTemplateController {
     @Resource(name = "deepseek")
     private ChatModel deepseekChatModel;
-    @Resource(name = "qwen")
-    private ChatModel qwenChatModel;
-
     @Resource(name = "deepseekChatClient")
     private ChatClient deepseekChatClient;
-    @Resource(name = "qwenChatClient")
-    private ChatClient qwenChatClient;
-
-
-
-
-
-
-
-
     @Value("classpath:/prompttemplate/template.txt")
     private org.springframework.core.io.Resource userTemplate;
 
     /**
      * @Description: PromptTemplate基本使用，使用占位符设置模版 PromptTemplate
-     * @Auther: zzyybs@126.com
      * 测试地址
      * http://localhost:8006/prompttemplate/chat?topic=java&output_format=html&wordCount=200
      */
     @GetMapping("/prompttemplate/chat")
-    public Flux<String> chat(String topic, String output_format, String wordCount)
-    {
-        PromptTemplate promptTemplate = new PromptTemplate("" +
-                "讲一个关于{topic}的故事" +
-                "并以{output_format}格式输出，" +
-                "字数在{wordCount}左右");
-
+    public Flux<String> chat(String topic, String output_format, String wordCount) {
+        PromptTemplate promptTemplate = new PromptTemplate("" + "讲一个关于{topic}的故事" + "并以{output_format}格式输出，" + "字数在{wordCount}左右");
         // PromptTempate -> Prompt
-        Prompt prompt = promptTemplate.create(Map.of(
-                "topic", topic,
-                "output_format",output_format,
-                "wordCount",wordCount));
-
+        Prompt prompt = promptTemplate.create(Map.of("topic", topic, "output_format", output_format, "wordCount", wordCount));
         return deepseekChatClient.prompt(prompt).stream().content();
     }
-
-
-
-
 
     /**
      * @Description: PromptTemplate读取模版文件实现模版功能
@@ -79,8 +46,7 @@ public class PromptTemplateController
      * http://localhost:8006/prompttemplate/chat2?topic=java&output_format=html
      */
     @GetMapping("/prompttemplate/chat2")
-    public String chat2(String topic,String output_format)
-    {
+    public String chat2(String topic, String output_format) {
         PromptTemplate promptTemplate = new PromptTemplate(userTemplate);
 
         Prompt prompt = promptTemplate.create(Map.of("topic", topic, "output_format", output_format));
@@ -90,17 +56,14 @@ public class PromptTemplateController
 
 
     /**
-     *  @Auther: zzyybs@126.com
-     * @Description:
-     * 系统消息(SystemMessage)：设定AI的行为规则和功能边界(xxx助手/什么格式返回/字数控制多少)。
+     * @Description: 系统消息(SystemMessage)：设定AI的行为规则和功能边界(xxx助手/什么格式返回/字数控制多少)。
      * 用户消息(UserMessage)：用户的提问/主题
      * http://localhost:8006/prompttemplate/chat3?sysTopic=法律&userTopic=知识产权法
-     *
+     * <p>
      * http://localhost:8006/prompttemplate/chat3?sysTopic=法律&userTopic=夫妻肺片
      */
     @GetMapping("/prompttemplate/chat3")
-    public String chat3(String sysTopic, String userTopic)
-    {
+    public String chat3(String sysTopic, String userTopic) {
         // 1.SystemPromptTemplate
         SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate("你是{systemTopic}助手，只回答{systemTopic}其它无可奉告，以HTML格式的结果。");
         Message sysMessage = systemPromptTemplate.createMessage(Map.of("systemTopic", sysTopic));
@@ -118,12 +81,10 @@ public class PromptTemplateController
      * @Description: 人物角色设定，通过SystemMessage来实现人物设定，本案例用ChatModel实现
      * 设定AI为”医疗专家”时，仅回答医学相关问题
      * 设定AI为编程助手”时，专注于技术问题解答
-     * @Auther: zzyybs@126.com
      * http://localhost:8006/prompttemplate/chat4?question=牡丹花
      */
     @GetMapping("/prompttemplate/chat4")
-    public String chat4(String question)
-    {
+    public String chat4(String question) {
         //1 系统消息
         SystemMessage systemMessage = new SystemMessage("你是一个Java编程助手，拒绝回答非技术问题。");
         //2 用户消息
@@ -141,16 +102,10 @@ public class PromptTemplateController
      * @Description: 人物角色设定，通过SystemMessage来实现人物设定，本案例用ChatClient实现
      * 设定AI为”医疗专家”时，仅回答医学相关问题
      * 设定AI为编程助手”时，专注于技术问题解答
-     * @Auther: zzyybs@126.com
      * http://localhost:8006/prompttemplate/chat5?question=火锅
      */
     @GetMapping("/prompttemplate/chat5")
-    public Flux<String> chat5(String question)
-    {
-        return deepseekChatClient.prompt()
-                .system("你是一个Java编程助手，拒绝回答非技术问题。")
-                .user(question)
-                .stream()
-                .content();
+    public Flux<String> chat5(String question) {
+        return deepseekChatClient.prompt().system("你是一个Java编程助手，拒绝回答非技术问题。").user(question).stream().content();
     }
 }
